a54bfc69721eb2c8ec4642cc8ce15c9e8553ec6b
Varun Naik <Varunnaik2001@gmail.com>
2023-08-28 02:16:38 -0500

    added sortKey to Arkham API call

--- fileCreator.js
+++ fileCreator.js
@@ -1,20 +1,18 @@
 import * as fs from 'fs';
 
-const objectToCSVRow = function(dataObject) {
+const headers = {'fromAddress':8};
+const createCSVRow = function(dataObject) {
 
     /*
     Creating Headers
     */
 
-    console.log('Data Object ' + dataObject);
-
     var dataArray = new Array;
 
     for (var o in dataObject) {
 
-        console.log( o + '|||' + dataObject[o] )
-
         if(!(typeof dataObject[o] == 'object')){
+
 
             var innerValue = dataObject[o]===null?'':dataObject[o].toString();
             var result = innerValue.replace(/"/g, '');
@@ -26,23 +24,25 @@ const objectToCSVRow = function(dataObject) {
         /*
         Logic to deal with nested objects
         */
+
+        
         else{
 
+ 
             for (var m in dataObject[o]) {
 
-                console.log( m + '|||' + dataObject[o][m] )
                 var innerValue = dataObject[o][m]===null?'':dataObject[o][m].toString();
                 var result = innerValue.replace(/"/g, '');
                 result = '' + result + ',';
                 dataArray.push(result);
         
-            }
+            } 
 
         }
 
     }
 
-  //  console.log(dataArray.join(' ')+'\r\n');
+    console.log(dataArray.length);
     return dataArray.join(' ') + '\r\n';
 }
 
@@ -52,7 +52,7 @@ const objectToCSV = function(dataObject){
         let data = dataObject[key];
     
 
-        let dataToAdd = objectToCSVRow(data);
+        let dataToAdd = createCSVRow(data);
         
         fs.appendFile('arkham-leads.txt', dataToAdd, function (err) {
             if (err) console.log(err);
@@ -64,3 +64,4 @@ const objectToCSV = function(dataObject){
 }
     
 export { objectToCSV };
+
--- index.js
+++ index.js
@@ -6,17 +6,25 @@ import {objectToCSV} from "./fileCreator.js";
 axios.defaults.headers.common['API-Key'] = 'R2dr5jjQAxHwg4LMc5RSGgfNvpN0uXGE';
 
 /*
-for complete list of parameters and parameter configurations, check the Arkham API notion page in README.md 
+
+base: which protocols to search 
+flow: transaction value "in" or "out" of the protocol
+limit: how many results to return 
+usdGte: USD value filter for transactions
+sortKey: "time" "value" or "usd" to sort transactions 
+
+For complete list of parameters and parameter configurations, check the Arkham API notion page in README.md 
 */
 
 axios({
     method: "get",
     url: "https://api.arkhamintelligence.com/transfers",
     params: {
-      base: "aave",
+      base: "aave,compound,notional-finance,morpho,venus",
       flow: "in",
-      limit: "3",
+      limit: "10",
       usdGte:"1000000",
+      sortKey:"usd",
 
     },
   }).then((value)=>{objectToCSV(value.data.transfers)});
