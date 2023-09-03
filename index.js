cd634078f2c2f7c219697bf6365434d30339c468
Varun Naik <Varunnaik2001@gmail.com>
2023-09-02 23:21:20 -0500

    changes to API request parameters

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ base: the entities to search
 flow: transactions "in" or "out" of the entity
 limit: how many results to return 
 usdGte: USD value filter for transactions
-timeGte: UNIX filter for transactions
+timeLast: UNIX filter for transactions
 sortKey: "time" "value" or "usd" to order transactions 
 
 For complete list of parameters and parameter configurations, check the Arkham API notion page in README.md 
@@ -25,8 +25,7 @@ axios({
       flow: "in",
       limit: "1000",
       usdGte:"1000000",
-      timeGte:"1684832820",
-      sortKey:"usd",
+      timeLast:"90d",
     },
   }).then((value)=>{objectToCSV(value.data.transfers)})
     .catch(error => console.log(error));
